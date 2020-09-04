e644fb540676cb1862d429eb828d340839294e8e
Chris Clark <chris@qalang.com>
2020-09-04 18:12:29 +0800

    Initial Commit

---
+++ 2020_0704_ExpandingLists/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  2020_0704_ExpandingLists
+//
+//  Created by Chris Clark on 2020/9/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 2020_0704_ExpandingLists/_020_0704_ExpandingListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _020_0704_ExpandingListsApp.swift
+//  2020_0704_ExpandingLists
+//
+//  Created by Chris Clark on 2020/9/4.
+//
+
+import SwiftUI
+
+@main
+struct _020_0704_ExpandingListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
